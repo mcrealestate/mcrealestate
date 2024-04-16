7d4590c65ab8f8134d3917fb328557740af6e255
Anna-Maria Christodoulou <62432130+MuTecEn@users.noreply.github.com>
2024-04-16 12:10:55 +0200

    house7

--- conversion.ps1
+++ conversion.ps1
@@ -1,7 +1,7 @@
-$IN_DIR = "C:\Users\annammc\OneDrive - Universitetet i Oslo\Documents\GitHub\images\House6\"
+$IN_DIR = "C:\Users\annammc\OneDrive - Universitetet i Oslo\Documents\GitHub\images\House7\"
 $EXT = "*.jpg"
 $LOGO_DIR = "C:\Users\annammc\OneDrive - Universitetet i Oslo\Documents\GitHub\images\logoV2.png"
-$OUT_DIR = "C:\Users\annammc\OneDrive - Universitetet i Oslo\Documents\GitHub\marios-realestate.gr\assets\images\House6"
+$OUT_DIR = "C:\Users\annammc\OneDrive - Universitetet i Oslo\Documents\GitHub\marios-realestate.gr\assets\images\House7"
 $LOGO_SIZE = "700,700" #for house1,4= 700, house2 = 200, house3 + 4 + 5 = 400, house 6 = 700
 # $PREFIX="h1"
 
